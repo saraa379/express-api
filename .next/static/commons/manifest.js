37a507391808689b4c05b9308fb2830b74003e2d
sara <saraa379@yahoo.com>
2018-09-19 11:26:09 +0200

    add book page layout updated

--- .next/static/commons/manifest.js
+++ .next/static/commons/manifest.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "43c4112f155fe46e6821"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "8ca7df86bd5a3b0dd25e"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -755,7 +755,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles\\pages\\_error.js","2":"bundles\\pages\\_app.js","4":"bundles\\pages\\index.js"}[chunkId]||chunkId) + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles\\pages\\_error.js","2":"bundles\\pages\\_app.js"}[chunkId]||chunkId) + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
--- pages/create.js
+++ pages/create.js
@@ -1,13 +1,173 @@
-import Page from '../components/page'
+//Main page
+import React, { Component } from 'react';
+import Menu2 from '../components/Menu2';
+import Header from '../components/Header';
 
-const page = () => (
-    <div>
-        <div>
-            <h2>Create Book Page</h2>
-        </div>
-        <style jsx>{`
-        `}</style>
-    </div>
-)
+class Create extends Component {
+	constructor(props) {
+			super(props);
+			this.state = {currentTab: "Add book", 
+						  book: {id: 0,
+						  		 volumeInfo: {
+						  		 	title: "",
+						  		 	authors: [],
+						  		 	publishedDate: "",
+						  		 	description: ""
+						  		 }
+						  }
+			};
+			this.tabClick = this.tabClick.bind(this);
+	}
 
-export default Page(page)
+	tabClick(ind) {
+    //console.log('Wrapper: Click happened: ' + ind);
+    	this.setState({currentTab: ind});
+  	}
+	render() {
+		return (
+		  <div className = 'rootDiv'>
+		  		<Header></Header>
+			    <Menu2 chosenTab={this.state.currentTab} clickEvent={this.tabClick}></Menu2>
+			    <div className="book">
+			    	<div id="form_content">
+						<form action="">
+							<input type = "text" id = "btitle" placeholder="Book title" />
+							<input type = "text" id = "bauthor" placeholder="Book author" />
+							<input type = "text" id = "bdate" placeholder="Published date" />
+							<textarea id="bdesc" placeholder="Description"
+								rows="7"></textarea>
+							<button class="btn"
+									id="btnAdd"> Add book </button>
+						</form>
+ 					</div>//end of form content
+			    </div>
+			    
+			    <style global jsx>{`
+			      * {
+			    		box-sizing: border-box;
+			    		margin: 0;
+		  			 	padding: 0;
+			    	}
+			      html, body {
+			      	 height: 100%;
+			      	 background: #202020;
+			      	 width: 100%;
+		  			 font-family: sans-serif;
+		  			 min-width: 200px;
+			      }
+			      body {
+			      	 background-image: 
+						url('/static/book1.jpg');
+						background-position: center;
+						background-repeat: no-repeat;
+						background-size: cover;
+			      }
+			      .rootDiv {
+			      		height: 100%;
+			      		width: 100%;
+			      }
+			      .title {
+			    		height: 200px;
+			    		display: flex;
+			    		flex-flow: column wrap;
+			    		align-items: center;
+			    	}
+			    	h1 {
+			    		text-align: center;
+			    		align-self: center;
+			    		display: inline;
+			    		margin: auto;
+			    		background-color: black;
+			    		text-transform: uppercase;
+			    		color: white;
+			    		padding: 5px 10px 5px 10px;
+			    		opacity: 0.5;
+			    	}
+			    	nav {
+			    		height: 45px;
+			    		background-color: #444444;
+			    		display: flex;
+			    		flex-flow: row nowrap;
+			    		align-items: center;
+			    		justify-content: center;
+			    		margin-bottom: 30px;
+			    	}
+			    	a {
+			    		padding-right: 10px;
+			    		padding-left: 10px;
+			    		padding-top: 13px;
+			    		padding-bottom: 13px;
+			    		text-decoration: none;
+						outline: none;
+						color: white;
+			    	}
+			    	a:hover {
+			    		background-color: grey;
+			    	}
+			    	.active {
+			    		background-color: grey;
+			    	}
+			    	.bookList {
+			    		color: white;
+			    		width: 100%;
+			    		display: flex;
+			    		flex-flow: column nowrap;
+			    		justify-content: center;
+			    		align-items; center;
+			    		margin-top: 30px;
+			    		margin-bottom: 30px;
+			    	}
+			    	.book {
+			    		background-color: black;
+			    		opacity: 0.7;
+			    		margin: 10px;
+			    		padding: 20px 10px 20px 10px;
+			    		width: 80%;
+			    		align-self: center;
+			    		margin: auto;
+			    	}
+			    	.book p {
+			    		padding-bottom: 5px;
+			    	}
+			    	#form_content {
+						padding: 20px;
+					}
+					input {
+						 outline: none;
+						 transition: .2s all linear;
+						 display: flex;
+						 padding: 8px;
+						 width: 100%;
+						 max-width: 500px;
+						 margin: 5px auto;
+					}
+					textarea {
+						 outline: none;
+						 transition: .2s all linear;
+						 display: flex;
+						 padding: 8px;
+						 width: 100%;
+						 margin: 5px auto;
+						 max-width: 500px;
+					}
+					form {
+						display: flex;
+						flex-flow: column wrap;
+						justify-content: center;
+					}
+					button {
+						 color: black;
+						 border-radius: 5px;
+						 font-size: 16px;
+						 line-height: 100%;
+						 padding: 5px 10px 5px 10px;
+						 width: 8em;
+						 margin: 5px auto;
+					}
+		    	`}</style>
+		  </div>
+  		);//end of return
+	} //end of render
+} //end of component
+
+export default Create;
